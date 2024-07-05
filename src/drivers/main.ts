f3b6ab3aa7e05c08279edb9a1abea2ba66747240
sajjad isvand <sajjadzibaee102@gmail.com>
2024-07-05 19:15:54 +0330

    feat: Add Zibal payment driver

--- src/drivers/main.ts
+++ src/drivers/main.ts
@@ -1,9 +1,11 @@
 import { ZarinPalDriver } from './zarinpal/zarinpal'
 import { IdPayDriver } from './idpay/idpay'
+import { ZibalDriver } from './zibal/zibal'
 
 const drivers = {
   zarinpal: ZarinPalDriver,
-  idPay: IdPayDriver
+  idPay: IdPayDriver,
+  zibal: ZibalDriver
 }
 
 export type Drivers = keyof typeof drivers
@@ -12,7 +14,7 @@ export type DriverInstance<T extends Drivers> = InstanceType<(typeof drivers)[T]
 
 export function getPaymentDriver<T extends Drivers>(name: T): DriverInstance<T> {
   const driverClass = drivers[name] as DriverInstance<T>
-  if (!driverClass) throw new Error('invalid driver name')
+  if (!driverClass) throw new Error(`Driver ${name} not found`)
 
   // @ts-ignore
   return new driverClass()
