fb4d3a6b32c7427507183099d35532644704c561
sajjad isvand <sajjadzibaee102@gmail.com>
2025-08-19 14:40:50 +0330

    add Paystar driver export to easypay module

--- src/easypay.ts
+++ src/easypay.ts
@@ -2,3 +2,4 @@ export { getPaymentDriver } from './drivers/main'
 export * from './drivers/zarinpal'
 export * from './drivers/idpay'
 export * from './drivers/zibal'
+export * from "./drivers/paystar"
